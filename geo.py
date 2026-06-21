ede5800ec28fc2a24d4d0c89f5c80f0fc932e1c3
new493370 <new493370@gmail.com>
2026-06-21 14:26:31 +0330

    Update geo.py

--- geo.py
+++ geo.py
@@ -1,3 +1,4 @@
+# geo.py
 import requests
 from cache import load_geo_cache, save_geo_cache
 
@@ -5,28 +6,14 @@ API_TIMEOUT = 5
 
 def geo_lookup(ip):
     cache = load_geo_cache()
-
     if ip in cache:
         return cache[ip]
-
     try:
-        r = requests.get(
-            f"https://ipwho.is/{ip}",
-            timeout=API_TIMEOUT
-        )
-
+        r = requests.get(f"https://ipwho.is/{ip}", timeout=API_TIMEOUT)
         data = r.json()
-
-        result = {
-            "country": data.get("country"),
-            "asn": data.get("connection", {}).get("asn"),
-            "provider": data.get("connection", {}).get("isp")
-        }
-
+        result = {"country": data.get("country"), "asn": data.get("connection", {}).get("asn"), "provider": data.get("connection", {}).get("isp")}
         cache[ip] = result
         save_geo_cache(cache)
-
         return result
-
     except:
         return {}
